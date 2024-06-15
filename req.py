b1723f3f95b54481bab88d16c555b43fcfd11c00
rianaditro <rianaditro@gmail.com>
2024-06-15 08:16:19 +0700

    add dati2ppk and jnsppk

--- req.py
+++ req.py
@@ -11,15 +11,38 @@ def get_list(payload: dict) -> str:
     r = requests.post(url, json=payload)
     return r.text
 
-def parse_get_list(data: str) -> list:
+def parse_get_list(data: str, dati2ppk:str) -> list:
     data = json.loads(data)
     result = []
-
+    print(f"{data['totalItem']} of item found from {dati2ppk}")
     for item in data['row']:
         jnsppk = item['jnsppk']
         kdppk = item['kdppk']
-        item_data = {"kdppk": kdppk, "jnsppk": jnsppk}
+        item_data = {"kdppk": kdppk, "jnsppk": jnsppk, "dati2ppk": dati2ppk, "totalItem": data["totalItem"]}
         result.append(item_data)
+    return result
+
+def get_list_by_dati2pplk(list_dati2ppk: list) -> list:
+    result = []
+    for item in list_dati2ppk:
+        payload = {"params":{"sort":{},
+                        "search":{},
+                        "pagination":{"start":0,
+                                #    "totalItemCount":"3",
+                                    "number":999
+                                #    ,"numberOfPages":1
+                                    }},
+            "propppk":"13",
+            "dati2ppk":item['id'],
+            # "jnsppk":item['jnsppk'],
+            "jarakterdekat":0}
+        # payload = {"propppk": 13, "dati2ppk": item['id']}
+        data = get_list(payload)
+        data = parse_get_list(data, item['name'])
+        print(f"for {item} get {len(data)} items")
+        for i in data:
+            result.append(i)
+    print(f"total items are {len(result)}")
     return result
 
 def get_data(payload: dict) -> dict:
@@ -48,28 +71,61 @@ def parse_get_data(data: dict) -> dict:
             result.update(data_dokter)
     return result
 
+def convert_jnsppk(code: str) -> str:
+    if code == "R":
+        return "Rumah Sakit"
+    elif code == "P":
+        return "Puskesmas"
+    elif code == "U":
+        return "Dokter Praktik Perorangan"
+    elif code == "G":
+        return "Dokter Gigi"
+    elif code == "S":
+        return "Klinik Utama"
+    elif code == "B":
+        return "Klinik Pratama"
+    elif code == "A":
+        return "Apotek"
+
 
 if __name__ == "__main__":
-    payload = {"params":
-           {"sort":{},
-            "search":{},
-            "pagination":{
-                "start":0,"totalItemCount":"831","number":831,"numberOfPages":84}
-            },"propppk":13,"dati2ppk":"","jarakterdekat":0
-            }
+
+    dati2ppk = [
+    {
+        "id": "0176",
+        "name": "KAB. KULON PROGO"
+    },
+    {
+        "id": "0177",
+        "name": "KAB. BANTUL"
+    },
+    {
+        "id": "0178",
+        "name": "KAB. GUNUNG KIDUL"
+    },
+    {
+        "id": "0179",
+        "name": "KAB. SLEMAN"
+    },
+    {
+        "id": "0180",
+        "name": "KOTA YOGYAKARTA"
+    }
+]
     
-    all_data = get_list(payload)
-    data_list = parse_get_list(all_data)
+    all_data = get_list_by_dati2pplk(dati2ppk)
 
     table_data = []
     
-    for i, data in enumerate(data_list):
-        print(f"Processing {i+1} of {len(data_list)}")
-        if data['jnsppk'] != 'R':
-            details = get_data(data)
-            rows = parse_get_data(details)
-            table_data.append(rows)
-            time.sleep(1)
+    for i, data in enumerate(all_data):
+        print(f"Processing {i+1} of {len(all_data)}")
+        details = get_data(data)
+        rows = parse_get_data(details)
+        rows['kabupaten/kota'] = data['dati2ppk']
+        rows['jenis faskes'] = convert_jnsppk(data['jnsppk'])
+        print(rows)
+        table_data.append(rows)
+        time.sleep(1)
 
     df = pd.DataFrame(table_data)
     df.to_excel("data.xlsx", index=False)
